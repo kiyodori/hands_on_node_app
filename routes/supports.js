7511059cc39f4cac4974f2e9af0b452300b323a2
kiyodori <kiyodori@yahoo.co.jp>
2018-01-22 08:38:24 +0900

    データベースの内容を表示

--- routes/supports.js
+++ routes/supports.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// const Support = require("../models/support");
 const models = require("../models");
 
 router.get('/:id', function(req, res, next) {
   const id = req.params.id;
-  const support = models.Support.findOne({ where: {id: id} });
-  // models.Support.findOne({ where: {id: id} }).then(support => {
-  //   const supportId = support.supportId;
-  //   const name = support.name;
-  //   res.render('supports', { supportId: supportId, name: name });
-  // })
-  res.render('supports', { supportId: support.supportId, name: support.name });
+  models.Support.findOne({
+    where: {id: id}
+  }).then(support => {
+    res.render('supports', { supportId: support.supportId, name: support.name });
+  }).catch(function(err) {
+    next();
+  })
 });
 
 module.exports = router;
